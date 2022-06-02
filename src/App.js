5e80eaee9bb60f1a59eae85feeffcb3de3a59fde
krishipaladiya <krishipaladiya@gmail.com>
2022-06-02 15:44:35 +0530

    02-06-2022

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
+          np
           rel="noopener noreferrer"
         >
           Learn React
