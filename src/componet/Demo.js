e1962490d072777ec51d8abb92adbe35150fed75
krishipaladiya <krishipaladiya@gmail.com>
2022-06-02 15:46:32 +0530

    02-06-2022

--- src/componet/Demo.js
+++ src/componet/Demo.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react'
 export default class Demo extends Component {
   render() {
     return (
-      <div>hello world</div>
+      <div><h2>hello world</h2></div>
     )
   }
 }
+export default Demo;
